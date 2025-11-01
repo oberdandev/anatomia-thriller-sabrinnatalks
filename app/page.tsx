72ac7fa91b9ee275e886ea86d6ec58f3aafca8a6
oberdan <oberdan.dev@gmail.com>
2025-11-01 20:17:23 -0300

    Atualiza formação na sessão sobre mim

--- app/page.tsx
+++ app/page.tsx
@@ -262,8 +262,15 @@ export default function Home() {
 
                 <div className="p-8 md:p-12">
                   <h3 className="text-3xl md:text-4xl font-bold mb-8 text-gray-100">Sabrinna Talks</h3>
-
                   <div className="space-y-6 text-gray-300 leading-relaxed">
+
+                      <div>
+                      <p className="text-lg text-gray-200 mb-2">
+                        <span className="text-red-500 font-semibold">Bacharel em Ciências Sociais</span>
+                      </p>
+                      <p>pela Universidade Federal do Amazonas (UFAM)</p>
+                    </div>
+
                     <div>
                       <p className="text-lg text-gray-200 mb-2">
                         <span className="text-red-500 font-semibold">Formada em Letras - Língua e Literatura Inglesa</span>
